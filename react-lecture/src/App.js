4c39e577091a3bedc67e408979ccf30618ae5dd3
Maddie Humphreys <heymaddieh@users.noreply.github.com>
2017-03-28 12:21:27 -0600

    another

--- react-lecture/src/App.js
+++ react-lecture/src/App.js
@@ -1,58 +1,76 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import MyFirstComponent from './MyFirstComponent'
+import List from './List'
 
-import List from './List';
 
+
+//***********************
 class App extends Component {
- 
-  constructor(props){  // used to keep track of changes
-    super(props)
+ constructor(props) {
+   super(props)
 
-    this.state = {
-      groceries: ['eggs', 'milk', 'cheese'],
-      chores: ['sweep', 'dishes', 'vacuum']
-      }  
-    }
-    removeItem(listName, itemName){
-      let targetList = this.state[listName];
+   this.state = {
+     divStyle: {
+       color: 'red'
+     },
+     groceries: ['eggs', 'milk', 'cheese'],
+     chores: ['sweep', 'dishes', 'vacuum']
+   }
+   this.removeItem = this.removeItem.bind(this);
+   this.handleClick = this.handleClick.bind(this);
 
-      let index = targetList.indexOf(itemName);
-      targetList.splice(index, 1);
+ }
 
-      let newState= this.state;
-      newState[listName] = targetList;
+ removeItem(listName, itemName) {
+   let targetList = this.state[listName];
 
-      this.setState(newState);
-    }
-  
+   let index = targetList.indexOf(itemName); //remove item from array
+   targetList.splice(index, 1);
 
-  handleClick(){ // function to change the color on click
-    this.setState({
-      divStyle: {
-        color: 'blue'
-      }
-    })
+   let newState = this.state;
+   newState[listName] = targetList;
 
-  }
+   this.setState(newState);
+ }
 
-  render(){
+ handleClick(){
+   this.setState({
+     divStyle: {
+       color: 'blue'
+     }
+   })
+ }
 
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+ render() {
 
+   return (
+       <div className="App">
+         <div className="App-header">
+           <img src={logo} className="App-logo" alt="logo" />
+           <h2>Welcome to React</h2>
+         </div>
+         {/*<MyFirstComponent name="Chase"/>*/}
+         <List list={['Chase','Jake','Maddie']} title="I am sitting next to"/>
 
+         <List listName='chores'
+         whenClicked={this.removeItem}
+         title="Chores"
+         list={this.state.chores} />
 
-        
-        <List title="Groceries" list={this.state.groceries}/>
-        <List title="Chores" list={this.state.chores}/>
-      </div>
-    );
-  }
+         <List listName='groceries'
+         whenClicked={this.removeItem}
+         title="Groceries"
+         list={this.state.groceries} />
+       
+       <div onClick={this.handleClick} style={this.state.divStyle}>
+         Click Me!
+       </div>
+       </div>
+   );
+ }
 }
+// ******^^^JSX^^^*********
 
 export default App;
